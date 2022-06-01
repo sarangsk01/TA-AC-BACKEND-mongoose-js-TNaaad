01ab852bbb72cfa3cccfd88db6c06c0cc6dd07bc
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-06-01 13:39:46 +0530

    Assignment I

---
+++ block-BNaacn/models/address.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var addressSchema = new Schema({
+  village: String,
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  pin: Number,
+  user: Schema.Types.objectId,
+  timestamps: String,
+});
+
+module.exports = mongoose.model('Address', addressSchema);
