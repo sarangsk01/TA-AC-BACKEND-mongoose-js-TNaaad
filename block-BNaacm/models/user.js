70f726a4dc0e3260568c2d284007e3810c2af2d6
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-06-01 13:11:43 +0530

    module.exports

---
+++ block-BNaacm/models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+  name: String,
+  email: { type: String, lowercase: true },
+  age: { type: Number, default: 0 },
+  favorites: [String],
+  marks: [Number],
+  password: { type: String, minlength: 5, maxlength: 15 },
+  createAt: { type: Date, default: 01 / 06 / 2022 },
+});
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;
